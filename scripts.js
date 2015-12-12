3eaadbdb1e5297dabced5f3e0d69edb40f190982
disorn <disornkwunchaithanya@hotmail.com>
2015-12-12 14:51:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,13 +3,14 @@ function changeFont(number){
   $("h").html("font-size", number);
   $("p").html("font-size", number);
 }
-
+$( document ).ready(function() {
 $('#startbutton').on('click', function(e){
   console.log('Function Started');
   $('#StoryBox').fadeOut();
   $('#startbutton').css('opacity', '0');
   $('.planet').css('opacity', '0.3');
   console.log('Function completed');
+});
 });
 
 
